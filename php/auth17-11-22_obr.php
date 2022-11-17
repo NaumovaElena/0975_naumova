d04442394b9196ec7a15dc817e283464ba4266bb
NaumovaElena <111225191+NaumovaElena@users.noreply.github.com>
2022-11-17 10:52:46 +0300

    авторизация php

---
+++ php/auth17-11-22_obr.php
@@ -0,0 +1,35 @@
+<?php
+//в хэдере открываем сессию
+session_start();
+//в хэдере отправляем требования по кодировке из браузера на сервер
+header('Content-Type: text/html; charset=utf-8');
+
+
+$mysqli = mysqli_connect("localhost", "qfktpikp_0975", "123456", "qfktpikp_0975");
+if ($mysqli == false) {
+// в php для конкатенации используется точка .
+  print("error" . mysqli_connect_error());
+} else {
+  //print("Соединение установлено успешно");
+  //trim - обрезка пробелов, mb_strtolower - понижение регистра.
+  $email = trim(mb_strtolower($_POST["email"]));
+  $pass = trim($_POST["pass"]);
+//шифрование пароля
+  $pass = password_hash("$pass", PASSWORD_DEFAULT);
+  
+  $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
+  //нужно преобразовать в ассоциативный массив, т.е. объект, у которого есть ключи
+  
+  $result = $result->fetch_assoc();
+  //var_dump($result["pass"]);
+  
+  if (password_verify($pass, $result["pass"])) {
+    echo "ok";
+    $_SESSION["name"] = $result["name"];
+    $_SESSION["lastname"] = $result["lastname"];
+    $_SESSION["email"] = $result["email"];
+    $_SESSION["id"] = $result["id"];
+  } else {
+    echo "not_found";
+  }
+}
